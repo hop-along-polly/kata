514a4bf446638511ca56de72dcf7c66694c2af90
Derek Drummond <ddrummond@cylance.com>
2019-05-11 14:24:05 -0500

    Finished tests for singly linked lists

--- lists/python/test_linked_list.py
+++ lists/python/test_linked_list.py
@@ -45,5 +45,44 @@ def test_ll_add_to_populated_list():
 def test_ll_remove_empty_list():
     subject = LinkedList()
 
-    with pytest.raises(NodeNotFoundException):
-        subject.remove('dne')
+    assert subject.remove('dne') == None
+    assert subject == []
+
+
+def test_ll_remove_first_item_in_list():
+    subject = LinkedList()
+    subject.add(Node('a'))
+
+    actual = subject.remove('a')
+    assert actual == 'a'
+    assert subject == []
+
+
+def test_ll_remove_item_in_middle_of_list():
+    subject = LinkedList()
+    subject.add(Node('a'))
+    subject.add(Node('b'))
+    subject.add(Node('c'))
+
+    assert subject.remove('b') == 'b'
+    assert subject == ['a', 'c']
+
+
+def test_ll_remove_last_item_in_the_list():
+    subject = LinkedList()
+    subject.add(Node('a'))
+    subject.add(Node('b'))
+
+    assert subject.remove('b') == 'b'
+    assert subject == ['a']
+
+
+def test_ll_remove_node_that_appears_multiple_times():
+    subject = LinkedList()
+    subject.add(Node('a'))
+    subject.add(Node('b'))
+    subject.add(Node('b'))
+    subject.add(Node('c'))
+
+    assert subject.remove('b') == 'b'
+    assert subject == ['a', 'b', 'c']
