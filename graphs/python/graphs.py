966a7ee514e001f4fce1f601ddbbbed1b99f7c4a
Derek Drummond <ddrummond@cylance.com>
2020-02-20 23:30:32 -0600

    Add Graph test harness

---
+++ graphs/python/graphs.py
@@ -0,0 +1,25 @@
+from typing import List
+
+class NodeDoesNotExistException(Exception):
+    """
+    You will not need to modify this it is provided as an example
+    of how you define a custom exception.
+    """
+    pass
+
+
+class Graph:
+    """
+    Implement an Undirectional Graph
+    """
+    class Node:
+        """
+        Implement a Graph Node. This is a subclass, since this node is
+        very specific for the graph implementation and should not be used
+        elsewhere in the code base.
+        """
+        def __init__(self, id: str, weight: int, **kwargs):
+            pass
+
+    def __init__(self, nodes: List[Graph.Node] = None):
+        pass
